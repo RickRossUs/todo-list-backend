39ece5afab3d7b2b9b3b123b208be9b96ada2974
ricardo sotolongo rosabal <ricardosr@estudiantes.uci.cu>
2024-05-16 06:21:32 -0400

    fix requirements

--- core/settings.py
+++ core/settings.py
@@ -186,6 +186,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFieldsStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
